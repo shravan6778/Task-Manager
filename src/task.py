5cfb397db7e4264e3dc729ae6063481a0f97c3f2
shravan6778 <jaligamshravan2004@gmail.com>
2026-07-06 16:15:09 +0530

    Whenever the task object is created using the task_id, title and description the Task class constructor intializes the instance variables like task_id, title, description, is_completed and created_at

--- src/task.py
+++ src/task.py
@@ -1,6 +1,12 @@
 from datetime import datetime
 
 class Task:
-    pass
+    def __init__(self, task_id: int, title: str, description: str = ""):
+        self.id = task_id
+        self.title = title
+        self.description = description
+        self.is_completed = False
+        self.created_at = datetime.now().strftime("%Y-%m-%d %H:%M")
+        
     
     
