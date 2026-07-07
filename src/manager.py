6627a256a5541e1c9ff06a6532b86186ee68731e
shravan6778 <jaligamshravan2004@gmail.com>
2026-07-07 15:43:47 +0530

    Whenever the TaskManager object is created the the attribute storage_file will initialized by tasks.json

--- src/manager.py
+++ src/manager.py
@@ -4,3 +4,7 @@ from typing import List
 from tabulate import tabulate
 from src.task import Task
 
+class TaskManager:
+    def __init__(self, storage_file: str = 'tasks.json'):
+        self.storage_file=storage_file
+    
